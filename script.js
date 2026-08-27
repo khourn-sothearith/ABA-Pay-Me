1b5d99a016a0ccaf545060d9a1d9505b4ddd6f0f
khourn-sothearith <khournsothearith465@gmail.com>
2026-08-27 14:22:18 +0700

    Update to new verion

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 // Open ABA payment link in new tab (so user stays on the page)
 function openABALink(type) {
   const urls = {
-    khr: 'https://pay.ababank.com/JFPJMo6DK7iGneS78',
-    usd: 'https://pay.ababank.com/kmnY6HysgbQQe3pn7'
+    khqr: 'https://pay.ababank.com/oRF8/gjryg4u6'
   };
   const url = urls[type];
   if (url) window.open(url, '_blank', 'noopener');
@@ -25,4 +24,22 @@ async function forceDownload(src, filename) {
   } catch {
     alert('Download failed. Please long-press the image and save manually.');
   }
+}
+// Auto copy account number by click on that number
+function copyAccountNumber(element) {
+    const accountNumber = element.innerText.trim();
+
+    navigator.clipboard.writeText(accountNumber)
+        .then(() => {
+            const originalText = element.innerText;
+
+            element.innerText = "Copied! ✓";
+
+            setTimeout(() => {
+                element.innerText = originalText;
+            }, 1500);
+        })
+        .catch((error) => {
+            console.error("Failed to copy:", error);
+        });
 }
